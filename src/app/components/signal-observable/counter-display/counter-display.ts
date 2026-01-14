229bb8c762fc05fd740488e2347af6063b40c8de
Rinkesh <rinkesh.web@gmail.com>
2026-01-14 12:04:05 +0530

    toSignal and toObservable

--- src/app/components/signal-observable/counter-display/counter-display.ts
+++ src/app/components/signal-observable/counter-display/counter-display.ts
@@ -1,15 +1,27 @@
-import { Component, effect, inject, signal } from '@angular/core';
+import { Component, computed, effect, inject, signal } from '@angular/core';
 import { CommonService } from '../../../core/services/common-service';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
+import { Message } from 'primeng/message';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-counter-display',
-  imports: [],
+  imports: [Message, AsyncPipe],
   templateUrl: './counter-display.html',
   styleUrl: './counter-display.css',
 })
 export class CounterDisplay {
   commonService = inject(CommonService);
   count = signal<number>(0);
+
+  // Computed
+  totalUsers = computed(() => this.commonService.users().length + 1);
+
+  // to observable
+  _totalUsers$ = toObservable(this.totalUsers);
+
+  // to Signal
+  _signalUsers = toSignal(this._totalUsers$);
 
   constructor() {
     effect(() => {
